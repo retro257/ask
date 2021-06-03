fa784eddfeaa4f26f81faeeb9496da8ece0b34c8
retro257 <caskinv2507@gmail.com>
2021-06-03 09:48:26 +0000

    Add all my files

--- qa/models.py
+++ qa/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+class QuestionManager(models.Manager):
+    def new(self):
+	return self.order_by('-added_at-')
+    def popular(self):
+        return self.order_by('-rating-') 
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='question_like_user')
-    class QuestionManager(models.Manager):
-        def new(self):
-	    return self.order_by('-added_at-')
-        def popular(self):
-            return self.order_by('-rating-')
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    question = models.OneToOneField(Question)
+    author = models.ForeignKey(User) 
