71042280579a2e3474963cfd00f929610419f248
retro257 <caskinv2507@gmail.com>
2021-06-05 19:18:15 +0000

    bruh

--- qa/views.py
+++ qa/views.py
@@ -1,3 +1,7 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from qa.models import Answer, Question 
 def test(requests, *args, **kwargs):
+    posts = Question.objects.new()
     return HttpResponse("OK")
+
