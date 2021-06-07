2f5eacb5d3f7c782455ffabd4c8d4b57e898dc3c
retro257 <caskinv2507@gmail.com>
2021-06-07 08:39:28 +0000

    Add all my files

--- qa/views.py
+++ qa/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-from qa.models import Answer, Question 
+from qa.models import Answer, Question
+from django.shortcuts import render 
 def test(requests, *args, **kwargs):
     posts = Question.objects.new()
     limit = 10
     page = requests.GET.get("page", 1)
     paginator = Paginator(posts, limit)
     page = paginator.page(page)
-    return HttpResponse("OK")
+    return render(requests, 'main.html', {paginator:paginator})
 
