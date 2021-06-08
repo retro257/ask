28b7d8f85b7a521c49b06d4d272cca6e727b9d88
Your Name <you@example.com>
2021-06-08 10:53:34 +0000

    AAAh

--- qa/models.py
+++ qa/models.py
@@ -1,21 +1,24 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models 
+from django.contrib.auth.models import User 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-added_at')
+        return self.order_by('-id')
     def popular(self):
         return self.order_by('-rating') 
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-    answer_set = models.BooleanField()
+    author = models.ForeignKey(User, default=1)
+    likes = models.ManyToManyField(User, related_name='questions', blank=True)
+    def get_absolute_url(self):
+        return reverse('question', kwargs={"id": self.id})
+    def __unicode__(self):
+        return self.title 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
-    question = models.OneToOneField(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, default=1)
