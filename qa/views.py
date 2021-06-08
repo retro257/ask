e451c16db539c39b51e7906380ccf733af9e0a2e
Your Name <you@example.com>
2021-06-08 09:50:32 +0000

    Add all my files

--- qa/views.py
+++ qa/views.py
@@ -3,11 +3,11 @@ from django.core.paginator import Paginator
 from qa.models import Answer, Question
 from django.shortcuts import render 
 def test(requests, *args, **kwargs):
-    posts = Question.objects.order_by('-added_at')
+    posts = Question.objects.order_by('-id')
     limit = requests.GET.get('limit', 10)
     page = requests.GET.get("page", 1)
     paginator = Paginator(posts, limit)
     paginator.baseurl = '/?page='
     page = paginator.page(page)
-    return render(requests, 'main.html', {paginator:paginator, question:Question})
+    return render(requests, 'main.html', {paginator:paginator, page:page})
 
